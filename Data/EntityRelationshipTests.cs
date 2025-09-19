5bbe50526f2c20c378143834380f5a557fccca40
Pc_Borsellino <gianmarcofi@gmail.com>
2025-09-19 10:33:42 +0200

    Aggiungere i file di progetto.

---
+++ Data/EntityRelationshipTests.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using EcdlBooking.Data;
+using EcdlBooking.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+[TestClass]
+public class EntityRelationshipTests
+{
+    [TestMethod]
+    public void ApplicationUser_RelationshipSchool_IsCorrect()
+    {
+        //Arrange Inizializzazione degli oggetti
+
+    }
+}
